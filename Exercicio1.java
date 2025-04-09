04d70b26e92cc3601c07d70ed623c7c2b499aeec
Davi Mira <davilopes0512@gmail.com>
2025-04-09 10:03:45 -0300

    Create Exercicio1.java

---
+++ Exercicio1.java
@@ -0,0 +1,10 @@
+//(Entrada + Processamento = Saída)
+
+//Faça um programa que mostre a mensagem "Olá, mundo!" na tela
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // Saída
+        System.out.println("Olá, mundo!");
+    }
+}
